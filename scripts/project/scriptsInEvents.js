6c51e0e87340ad4c374dfab379fd361708275e74
jorcidesign <jorci.design@gmail.com>
2023-08-24 07:41:46 -0500

    questionario de aves al 60%, primeras animaciones de feedback

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Equizzes_Event40_Act2(runtime, localVars)
+	{
+		alert("Inténtalo de nuevo")
+	},
+
+	async Equizzes_Event41_Act2(runtime, localVars)
+	{
+		alert("Sigue así")
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
