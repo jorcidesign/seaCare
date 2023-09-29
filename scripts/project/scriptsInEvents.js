99b7359ef989f24076ffb271ac05fa5219bebec2
jorcidesign <jorci.design@gmail.com>
2023-09-28 22:43:29 -0500

    ultimos cambios

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -3,6 +3,19 @@
 
 const scriptsInEvents = {
 
+	async Emain_Event9_Act4(runtime, localVars)
+	{
+		//editar aquí
+			alert("hola mundo")
+		
+	},
+
+	async Emain_Event14_Act1(runtime, localVars)
+	{
+		//Editar aquí
+		console.log("juego iniciado");
+	},
+
 	async Equizzes_Event46_Act2(runtime, localVars)
 	{
 		console.log("Inténtalo de nuevo")
